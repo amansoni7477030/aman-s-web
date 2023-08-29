b4f649d96ae289feb61d837a2b8a062ba10287cb
amansoni7477030 <65220576+amansoni7477030@users.noreply.github.com>
2023-08-29 15:27:53 +0530

    Update submit_form.php

--- submit_form.php
+++ submit_form.php
@@ -1,5 +1,7 @@
 <?php
+// Check if the form has been submitted using POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // If the form has been submitted, display a thank you message
     echo "Thank You! We will get back to you";
 }
 ?>
